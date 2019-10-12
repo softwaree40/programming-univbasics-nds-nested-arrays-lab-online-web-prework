8363f2c0ce2e1dd37dc7703078ea414b5c2eab01
relational-router-4343 <ajagunnaabiodun752@yahoo.com>
2019-10-12 01:05:15 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -34,6 +34,7 @@ end
 def matrix_update(matrix, row, column, new_value)
   # Update the matrix location at row and column to have the value of new_value
   # Return the updated matrix
-        return  matrix[0][0] = 'new_value'
+          matrix[0][0] = 'new_value'
+          return matrix
          
 end
